b5cd5c8dae43aa4f8731c9d97bd87f2e4043898f
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-03-12 14:08:42 -0300

    M1S08 exercicio 4

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+//Remova um elemento filho de um elemento pai na página HTML
+
+let container = document.getElementById('container');
+let removeP = document.getElementById('removeP');
+container.removeChild(removeP);
